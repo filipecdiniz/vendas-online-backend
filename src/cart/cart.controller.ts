76980de66c87092760851c11950cc84758dec982
filipe.diniz <filipe.diniz@ellevti.com.br>
2024-12-19 13:10:07 -0300

    Feature: Cart controller and service created!.

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { InsertProductDTO } from './DTOs/InsertProduct.dto'; 
+import { CartEntity } from './Entities/cart.entity';
+import { CartService } from './cart.service';
+import { UserID } from 'src/decorators/userID.decorator';
 
 @Roles(UserType.User)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+    constructor(private readonly cartService: CartService) { }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async insertProduct(@Body() insertProductDTO: InsertProductDTO, @UserID() userId: number): Promise<CartEntity> {
+        return await this.cartService.insertProduct(insertProductDTO, userId);
+    }
+}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CartEntity } from './Entities/cart.entity';
 import { Repository } from 'typeorm';
+import { InsertProductDTO } from './DTOs/InsertProduct.dto';
 
 @Injectable()
 export class CartService {
     constructor(
         @InjectRepository(CartEntity)
         private readonly cartRepository: Repository<CartEntity>
-    ){}
+    ) { }
+
+    async createCart(userId: number): Promise<CartEntity> {
+        return await this.cartRepository.save({
+            active: true,
+            userId
+        });
+    }
+
+    async verifyActiveCart(userId: number): Promise<CartEntity> {
+        const cart = await this.cartRepository.findOne({
+            where: { userId }
+        });
+
+        if (!cart) throw new NotFoundException(`Active cart not found!`);
+
+        return cart;
+    }
+
+    async insertProduct(insertProductDTO: InsertProductDTO, userId: number): Promise<CartEntity> {
+        const cart = await this.verifyActiveCart(userId).catch(async () => {
+            return this.createCart(userId)
+        });
+
+        return cart;
+    }
 }
